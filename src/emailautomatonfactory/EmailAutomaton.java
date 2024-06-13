923189bb8ac94224f16f91db2d23d3b5f8649145
Balasis_Ioannis <giovani1994a@gmail.com>
2024-06-13 16:20:57 +0300

    you create states when you you know the next possible states ; ...give in the constructor the next states even if empty;

--- src/emailautomatonfactory/EmailAutomaton.java
+++ src/emailautomatonfactory/EmailAutomaton.java
@@ -1,4 +1,15 @@
 package emailautomatonfactory;
 
+import emailschemafactory.EmailSchema;
+
+import java.util.List;
+
 public class EmailAutomaton {
+    List<State> allStatesQ;
+    List<State> initialStatesI;
+    List<State> finalStatesF;
+
+    public EmailAutomaton(EmailSchema emailSchema){
+
+    }
 }
