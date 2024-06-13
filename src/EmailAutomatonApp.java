ada7e2b78f0a6b17215ef58b66ca3f63f32223a4
Balasis_Ioannis <giovani1994a@gmail.com>
2024-06-13 09:48:06 +0300

    removing from app class

--- src/EmailAutomatonApp.java
+++ src/EmailAutomatonApp.java
@@ -2,7 +2,6 @@ import java.util.Set;
 
 public class EmailAutomatonApp {
     public static void main(String[] args) {
-        State tr=new State("q0","a", Set.of("q1","q2"));
-        System.out.println(tr);
+
     }
 }
