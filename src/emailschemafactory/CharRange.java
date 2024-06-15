8c533bfe27ca73a84cbcdccc1e1ff58232182b71
Balasis_Ioannis <giovani1994a@gmail.com>
2024-06-15 17:41:25 +0300

    ... T.T converted back to the record... I was wrong about its methods it can accept.

--- src/emailschemafactory/CharRange.java
+++ src/emailschemafactory/CharRange.java
@@ -1,21 +1,6 @@
 package emailschemafactory;
 
-public class CharRange {
-    private final char from;
-    private final char to;
-
-    public CharRange(char from, char to) {
-        this.from = from;
-        this.to = to;
-    }
-
-    public char from() {
-        return from;
-    }
-
-    public char to() {
-        return to;
-    }
+public record CharRange(char from, char to) {
 
     public boolean contains(char c) {
         return from <= c && c <= to;
